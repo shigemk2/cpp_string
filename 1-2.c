104713afb9486bd81dd9cc135f18d032cad2205d
shigemk2 <i.am.shige@gmail.com>
2015-04-25 00:57:58 +0900

    文字列の結合 その2

---
+++ 1-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char buf[8];
+    const char *s1 = "abc";
+    const char *s2 = "def";
+    const char *s3 = "ghijkl";
+    strcpy(buf, s1);
+    strcat(buf, s2);
+    strcat(buf, s3);
+    printf("%s\n", buf);
+    return 0;
+}
