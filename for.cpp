499f208b5cd115d23fffd09b680a07dd36cd2d9b
shigemk2 <i.am.shige@gmail.com>
2015-04-26 23:30:47 +0900

    Fix for

--- for.cpp
+++ for.cpp
@@ -5,5 +5,8 @@ void string() {
     str = new char[1000000000];
 }
 int main() {
-    string();
+    for (unsigned int i = 0; i < 1000000 ; i++) {
+        printf("i: %d\n", i);
+        string();
+    }
 }
