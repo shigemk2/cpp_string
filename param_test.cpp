1684dfdffb17fb9e847cd6899a7dc17b2dea645a
shigemk2 <i.am.shige@gmail.com>
2015-04-28 21:21:38 +0900

    引数の自動変換

---
+++ param_test.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+struct mystr {
+    char *str;
+    size_t len;
+
+    mystr() {
+        str = NULL;
+        *this = "";
+    }
+
+    mystr(const char *s) {
+        str = NULL;
+        *this = s;
+    }
+
+    mystr(const mystr &s) {
+        str = NULL;
+        *this = s;
+    }
+
+    ~mystr() {
+        printf("~mystr: %s\n", str);
+        delete[] str;
+    }
+
+    void printn() const {
+        printf("%s\n", str);
+    }
+
+    void set(const char *s, size_t newlen) {
+        char *old = str;
+        len = newlen;
+        str = new char[len + 1];
+        strcpy(str, s);
+        delete[] old;
+    }
+
+
+    mystr &operator+=(const char *s) {
+        set(str, len + strlen(s));
+        strcat(str, s);
+        return *this;
+    }
+
+    mystr &operator+=(const mystr &s) {
+        set(str, len + s.len);
+        strcat(str, s.str);
+        return *this;
+    }
+
+    mystr &operator=(const char *s) {
+        set(s, strlen(s));
+        return *this;
+    }
+
+    mystr &operator=(const mystr &s) {
+        set(s.str, s.len);
+        return *this;
+    }
+};
+
+void test(const mystr &s) {
+    s.printn();
+}
+
+int main() {
+    test("abc");
+}
