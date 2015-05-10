6f7d7896539fba4adfde9b4aaea9ddea17e856bd
shigemk2 <i.am.shige@gmail.com>
2015-05-10 17:24:33 +0900

    複素数Scala

--- complex.scala
+++ complex.scala
@@ -4,10 +4,13 @@ case class Complex(real: Int, imag: Int) {
   def -(that: Complex) = Complex(real - that.real, imag - that.imag)
 
   def str() = {
-    imag match {
-      case imag if imag > 0 => real + " + " + imag + "i"
-      case imag if imag == 0 => real
-      case imag if imag < 0 => {
+    (real, imag) match {
+      case (real, _) if real == 0 && imag == 1 => "i"
+      case (real, _) if real == 0 && imag == -1 => "-i"
+      case (real, _) if real == 0 => imag + "i"
+      case (_, imag) if imag > 0 => real + " + " + imag + "i"
+      case (_, imag) if imag == 0 => real
+      case (_, imag) if imag < 0 => {
         real + " - " + imag.abs + "i"
       }
     }
@@ -21,9 +24,11 @@ val c = a + b
 val d = a * b
 val e = a - b
 val f = b - a
+
 println(a.str)
 println(b.str)
 println(c.str)
 println(d.str)
 println(e.str)
 println(f.str)
+println(i.str)
