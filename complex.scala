adc089c3f80068c0c378420bc09f3caea6e58c6f
shigemk2 <i.am.shige@gmail.com>
2015-05-10 18:33:41 +0900

    複素数 Scala

--- complex.scala
+++ complex.scala
@@ -1,16 +1,19 @@
 case class Complex(real: Int, imag: Int) {
-  def +(that: Complex) = Complex(real + that.real, imag + that.imag)
-  def *(that: Complex) = Complex(real * that.real, imag * that.imag)
-  def -(that: Complex) = Complex(real - that.real, imag - that.imag)
+  def this(re: Int) = this(re, 0)
 
-  def str() = {
+  implicit def +(that: Complex) = Complex(real + that.real, imag + that.imag)
+  implicit def *(that: Complex) = Complex(real * that.real, imag * that.imag)
+  implicit def -(that: Complex) = Complex(real - that.real, imag - that.imag)
+
+  override def toString() = {
     (real, imag) match {
-      case (real, _) if real == 0 && imag == 1 => "i"
-      case (real, _) if real == 0 && imag == -1 => "-i"
-      case (real, _) if real == 0 => imag + "i"
-      case (_, imag) if imag > 0 => real + " + " + imag + "i"
-      case (_, imag) if imag == 0 => real
-      case (_, imag) if imag < 0 => {
+      case (real, imag) if real == 0 && imag == 1 => "i"
+      case (real, imag) if real == 0 && imag == -1 => "-i"
+      case (real, imag) if real == 0 && imag == 0 => "0"
+      case (real, imag) if real == 0 => imag + "i"
+      case (real, imag) if imag > 0 => real + " + " + imag + "i"
+      case (real, imag) if imag == 0 => real.toString
+      case (real, imag) if imag < 0 => {
         real + " - " + imag.abs + "i"
       }
     }
@@ -18,17 +21,16 @@ case class Complex(real: Int, imag: Int) {
 }
 
 val i = new Complex(0, 1)
-val a = new Complex(1, 3)
-val b = new Complex(3, 1)
+val a = new Complex(1, 2)
+val b = new Complex(3, 4)
 val c = a + b
 val d = a * b
 val e = a - b
-val f = b - a
+val f = a - a
 
-println(a.str)
-println(b.str)
-println(c.str)
-println(d.str)
-println(e.str)
-println(f.str)
-println(i.str)
+println(a)
+println(b)
+println(c)
+println(d)
+println(e)
+println(f)
