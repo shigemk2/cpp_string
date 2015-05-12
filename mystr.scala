bbcdba8d33328605fc416a3fab22fac5094c6f9a
shigemk2 <i.am.shige@gmail.com>
2015-05-12 20:50:41 +0900

    Fix

--- mystr.scala
+++ mystr.scala
@@ -1,8 +1,13 @@
 class MyStr(str: String) {
+  def this() = this("123")
+
   def println(): Unit = {
-    print(s"$str")
+    print(s"$str\n")
   }
 }
 
 val s = new MyStr("abc")
 s.println()
+
+val t = new MyStr()
+t.println()
